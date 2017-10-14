ee4118f875b37e57250ea1758a39dd96b764fde1
IIIIIIIIIU <32150313+IIIIIIIIIU@users.noreply.github.com>
2017-10-14 10:32:52 +0800

    Create T2FC.cpp

---
+++ 20171014/T2FC.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+	while(true)
+	{
+		system("T2maker.exe");
+		system("dawnBOOM.exe");
+		system("dawn.exe");
+		if(system("fc T2out1.txt T2out2.txt")) break;
+	}
+	return 0;
+}
