7a7ad5bf6f41ca54c6369428e07dfc8730dff1aa
IIIIIIIIIU <32150313+IIIIIIIIIU@users.noreply.github.com>
2017-10-25 21:10:17 +0800

    Create Codeforces877C.cpp

---
+++ 20171025/Codeforces877C.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    scanf("%d",&n);
+    printf("%d\n",n+n/2);
+    for(int i=2;i<=n;i+=2) printf("%d ",i);
+    for(int i=1;i<=n;i+=2) printf("%d ",i);
+    for(int i=2;i<=n;i+=2) printf("%d ",i);
+}
