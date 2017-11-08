fe7e9b0e0f7d7047e32f3cba6ac59f46385cbff5
IIIIIIIIIU <32150313+IIIIIIIIIU@users.noreply.github.com>
2017-11-08 19:33:06 +0800

    Create compare.cpp

---
+++ 20171108/contest/park/compare.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+	while(true)
+	{
+		system("datamaker.exe");
+		system("park.exe");
+		system("BOOM.exe");
+		if(system("fc ans1.txt ans2.txt"))
+			break;
+	}
+	return 0;
+}
