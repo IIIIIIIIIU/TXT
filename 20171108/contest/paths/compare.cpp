4972db99a696c610759318d03a54c80fbcb627e7
IIIIIIIIIU <32150313+IIIIIIIIIU@users.noreply.github.com>
2017-11-08 19:31:39 +0800

    Create compare.cpp

---
+++ 20171108/contest/paths/compare.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+	while(true)
+	{
+		system("datamaker.exe");
+		system("paths.exe");
+		system("BOOM.exe");
+		if(system("fc ans1.txt ans2.txt"))
+			break;
+	}
+}
